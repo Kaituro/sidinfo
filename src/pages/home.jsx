ab51ac5b543a5e1300a48208570196120ad50112
Valentin <valentin.merault@gmail.com>
2022-12-01 21:44:22 +0100

    Home + font

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,20 +2,26 @@ const Home = () => {
   document.title = "SexInfo";
 
   return (
-    <div className="h_box">
-      <div className="welcome-text">
-        <h1>
-          <span>Bienvenue</span>,
-        </h1>
-        <h2>
-          vous pouvez naviguer sur ce site pour vous amuser et vous renseigner
-          sur les maladies sexuellement transmisbiles.
-        </h2>
+    <div>
+      <div className="h_box">
+        <div className="welcome-text">
+          <h1>
+            <span>Bienvenue</span>,
+          </h1>
+          <h2>
+            vous pouvez naviguer sur ce site pour vous amuser et vous renseigner
+            sur les maladies sexuellement transmisbiles.
+          </h2>
+        </div>
+
+        <div className="img-container">
+          <img src="./image/logosida.svg" alt="marche pas" />
+        </div>
       </div>
 
-      <div className="img-container">
-        <img src="./image/logosida.svg" alt="marche pas" />
-      </div>
+      <div className="fil"></div>
+
+      <div className="protect"></div>
     </div>
   );
 };
