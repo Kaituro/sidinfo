08a45002b2ad2ab4d667f1504c60bb82d421b966
Kaituro <josian.riviere@etud.univ-jfc.fr>
2022-12-01 21:17:52 +0100

    contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,1 +1,23 @@
 import React from "react";
+
+const Contact = () => {
+    document.title = "SexInfo";
+  
+    return (
+      <div className="h_box">
+        <div>
+        <ul>
+        <li>Tel</li>
+        <li>Mail</li>
+        <li>Web</li>
+
+        <img src="image/logosida.svg" />
+        </ul>
+
+        </div>
+
+      </div>
+    );
+  };
+  
+  export default Contact;
