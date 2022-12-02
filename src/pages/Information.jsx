a24bfd779b0a5b850416734bbaabb4c8a461c9bf
Kaituro <josian.riviere@etud.univ-jfc.fr>
2022-12-02 01:13:11 +0100

    information alignement

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -12,9 +12,45 @@ const Information = () => {
 
   return (
     <div>
-      <div className="h_box">
-        <h1>Information</h1>
+      <div className="informationcorps">
+        
+        <div className="gaucheinfo">
+        <h2>Qu'est ce qu'une IST ?</h2>
+        <p></p>  
+        </div>
+        
+    <div className="droiteinfo">
+<h2>Qu'est ce que le SIDA ?</h2>
+        <p></p>
+
+    </div>
+        
+    <div className="gaucheinfo">
+       <h2>Différences entre sida et VIH :</h2>
+        <p></p>
+        <h3>Pourquoi ne pas confondre ?</h3>
+        <p></p>
+    </div>
        
+<div className="droiteinfo">
+<h2>L'importance du dépistage ?</h2>
+        <p></p>
+</div>
+        
+    <div className="gaucheinfo">
+      <h2>Comment se protéger ?</h2>
+        <p></p>
+    </div>
+        
+    <div className="droiteinfo">
+      <h2>Les soins inexistants ?</h2>
+        <p></p>
+    </div>
+        
+    <div className="gaucheinfo">
+      <h2>Comment vivre avec ?</h2>
+        <p></p>
+    </div>      
         
       </div>
     </div>
