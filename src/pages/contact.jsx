fe5f6de506176331154f405e606b9cab31c12e2e
Valentin <valentin.merault@gmail.com>
2022-12-02 01:12:48 +0100

    Footer + contact responsive

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 
 const Contact = () => {
-    document.title = "SexInfo";
-  
-    return (
-      <>
-       
-        <div className="contactinfo"> 
-        <div className="welcome-text">  
-         <h1 className="titrecontact">Nous contacter</h1>
-        <ul>
-        <li><span>Tel :</span> 0 800 00 69 07</li>
-        <li><span>Mail :</span> jsp.test@gmail.com</li>
-        <li> <span>Web :</span> www.test.html.sida</li>        
-        </ul>
-        </div>
-     
-<img  className="img-container" src="image/logosida.svg" />
+  document.title = "SexInfo";
+
+  return (
+    <>
+      <div className="contactinfo">
+        <div className="welcome-text">
+          <h1 className="titrecontact">Nous contacter</h1>
+          <ul>
+            <li>
+              <span>Tel :</span> 0 800 00 69 07
+            </li>
+            <li>
+              <span>Mail :</span> jsp.test@gmail.com
+            </li>
+            <li>
+              {" "}
+              <span>Web :</span> www.test.html.sida
+            </li>
+          </ul>
         </div>
 
-        </>
-    );
-  };
-  
-  export default Contact;
+        <img className="img-container" src="image/logosida.svg" />
+      </div>
+    </>
+  );
+};
+
+export default Contact;
